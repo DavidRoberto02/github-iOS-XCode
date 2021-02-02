c64a93b117db713d559b81a1a4296ddb9afc4f78
David Saucedo <David.Sauced@outlook.com>
2021-02-01 21:40:44 -0600

    Playground
    Actividad 1

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+//Asignaciòn de la variable.
+var v1 = "esto es Swift"
+//Asignaciòn de la constante 1 con el valor de 4.
+let c1 = 4
+
+//Asignaciòn de la cadena tipo String.
+var cadena: String = String()
+
+//Valor mutable y asignaciònes formales
+cadena = "Hola mundo " + v1
+
+print("El contenido es: \(cadena)")
+
+print("El valor de la constante es: \(c1)")
